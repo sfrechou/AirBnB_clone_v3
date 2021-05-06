70639a4d9628db1b076496bef539d4e5b8baa894
Soledad Frechou <solefrechou@hotmail.com>
2021-05-06 21:34:52 +0000

    Update state view: states/state_id

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
 """Default RESTFul API for State"""
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, abort
 from models import storage
 from models.state import State
 
 
-@app_views.route('/states',  methods=['GET'])
+@app_views.route('/states/',  methods=['GET'])
 def get_states():
     """Retrieves the list of all State objects"""
     all_obj = storage.all(State)
     single_objs = []
     for key, value in all_obj.items():
         single_objs.append(value.to_dict())
-    print(single_objs)
     return jsonify(single_objs)
 
 @app_views.route('/states/<state_id>',  methods=['GET'])
-def get_state_id():
-    """Retrieves the list of all State objects"""
+def get_state_id(state_id):
+    """Retrieves a State object by id"""
     all_obj = storage.all(State)
-    single_objs = []
+    new_dict = {}
     for key, value in all_obj.items():
-        single_objs.append(value.to_dict())
-    print(single_objs)
-    return jsonify(single_objs)
+        if state_id in key:
+            new_dict = value.to_dict()
+            return jsonify(new_dict)
+    abort(404)
