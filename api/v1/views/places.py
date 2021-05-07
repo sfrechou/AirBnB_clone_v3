83b20a5d71e25896fa795bc9434d95c4032c25b3
Soledad Frechou <solefrechou@hotmail.com>
2021-05-07 23:53:14 +0000

    Fix places.py User

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -34,7 +34,7 @@ def city_id_places(city_id):
                     user = storage.get(User, user_id)
                     if user:
                         if "name" in new_dict.keys():
-                            new_place = User(**new_dict)
+                            new_place = Place(**new_dict)
                             storage.new(new_place)
                             storage.save()
                             return jsonify(new_place.to_dict()), 201
