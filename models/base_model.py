2a892c90cb3e835e37904b136e5f57cba7af49a6
sfrechou <solefrechou@hotmail.com>
2021-05-08 16:39:14 +0000

    Add basemodel and user

--- models/base_model.py
+++ models/base_model.py
@@ -69,9 +69,6 @@ class BaseModel:
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
 
-        if models.storage_t == 'db':
-            del new_dict["password"]
-
         return new_dict
 
     def delete(self):
